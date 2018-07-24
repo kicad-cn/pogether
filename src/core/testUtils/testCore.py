c48c9c157b8cdd38f900aacdfb2afbef6024ee5d
LiuHancheng <2463765697@qq.com>
2018-07-24 14:06:49 +0800

    修改文件上传单元测试

--- src/core/testUtils/testCore.py
+++ src/core/testUtils/testCore.py
@@ -7,6 +7,7 @@ import pickle
 import shutil
 import tempfile
 import codecs
+import random
 
 # Create your tests here
 
@@ -16,23 +17,32 @@ with open("./core/testUtils/sample.po") as fp:
     samplePo.write(bytes(fp.read(), 'utf-8'))
     samplePo.seek(0)
 
+print("m:",MEDIA_ROOT)
 
-@override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class testCore(TestCase):
-    def setUp(self):
+    @classmethod
+    @override_settings(MEDIA_ROOT=MEDIA_ROOT)
+    def setUpClass(cls):
         FileObj = File(samplePo, name='Sample.po')
         docobj = Document(Name='Sample.po',
-                          Content=samplePo.read(), PoFile=FileObj)
+                          PoFile=FileObj)
         docobj.save()
         PoFile: File = docobj.PoFile
         fileName = samplePo.name
-        self.PoFile: polib.POFile = polib.pofile(fileName)
-        for entry in self.PoFile:
+        cls.PoFile: polib.POFile = polib.pofile(fileName)
+        for entry in cls.PoFile:
             POEntry(Raw=pickle.dumps(entry),
-            Doc=docobj,
-            Translated = entry.translated()
-            ).save()
+                    Doc=docobj,
+                    Translated=entry.translated(),
+                    Msgid=entry.msgid,
+                    Msgstr=entry.msgstr
+                    ).save()
+    
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
+    @override_settings(MEDIA_ROOT=MEDIA_ROOT)
     def testFileUpload(self):
         """
         测试文件是否上传到了指定位置
@@ -46,6 +56,19 @@ class testCore(TestCase):
         """
         untranslated = POEntry.objects.filter(Translated=False).count()
         translated = POEntry.objects.filter(Translated=True).count()
-        self.assertEqual(len(self.PoFile.translated_entries()),translated)
-        self.assertEqual(len(self.PoFile.untranslated_entries()),untranslated)
-        self.assertEqual(self.PoFile.percent_translated(),100-int(untranslated*100/translated))
+        self.assertEqual(len(self.PoFile.translated_entries()), translated)
+        self.assertEqual(len(self.PoFile.untranslated_entries()), untranslated)
+        self.assertEqual(self.PoFile.percent_translated(),
+                         100-int(untranslated*100/translated))
+
+    def testMsg(self):
+        """
+        测试原文本的读取
+        """
+        index = random.randint(0, 100)
+        randomMsgid = self.PoFile[index].msgid
+        self.assertEqual(
+            len(POEntry.objects.filter(Msgid__exact=randomMsgid)), 1)
+    
+
+    
