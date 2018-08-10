dd9b780ed791ec770e48dff8612a9cc1daa0f36a
LiuHancheng <2463765697@qq.com>
2018-08-10 23:31:01 +0800

    修复手滑弄乱大小写的错误

--- src/core/management/commands/addpo.py
+++ src/core/management/commands/addpo.py
@@ -3,68 +3,66 @@ from core.models import Document, POEntry
 from django.core.files import File
 from django.db import transaction
 import os
-import json
 import logging
 import polib
 import pickle
 import sys
 
-class command(BaseCommand):
+class Command(BaseCommand):
     help = '向数据库内添加po文件'
 
     def add_arguments(self, parser):
-        parser.add_argument('-i', '--file', type=str, help='声明要添加的po文件', required=true)
+        parser.add_argument('-i', '--file', type=str, help='声明要添加的po文件', required=True)
         parser.add_argument('-u', '--update', action='store_true',
-                                help='是否执行更新操作', dest='isupdate')
-        parser.add_argument('--poname',  type=str,
-                        help='手动设定po文档名称', dest='poname')#default none
+                                help='是否执行更新操作', dest='isUpdate')
+        parser.add_argument('--poName',  type=str,
+                        help='手动设定po文档名称', dest='poName')#default None
 
     def handle(self, *args, **opt):
         if opt['verbosity']==0:
-            logging.basicconfig(level=logging.critical)
+            logging.basicConfig(level=logging.CRITICAL)
         elif  opt['verbosity']==1:
-            logging.basicconfig(level=logging.error)
+            logging.basicConfig(level=logging.ERROR)
         elif  opt['verbosity']==2:
-            logging.basicconfig(level=logging.info)
+            logging.basicConfig(level=logging.INFO)
         elif  opt['verbosity']==3:
-            logging.basicconfig(level=logging.debug)
-        if opt['poname'] == none:
-            basename = os.path.splitext(os.path.basename(opt['file']))[0]
+            logging.basicConfig(level=logging.DEBUG)
+        if opt['poName'] == None:
+            BaseName = os.path.splitext(os.path.basename(opt['file']))[0]
         else:
-            basename = opt['poname']
-        pofile = polib.pofile(opt['file'])
-        docobj = none
-        if document.objects.filter(name__exact=basename).count() > 0: # check whether this doc has been inserted or not
-            docobj = document.objects.get(name__exact=basename)
-            if not opt['isupdate']:
-                logging.warning("po文件已存在，跳过添加")
+            BaseName = opt['poName']
+        PoFile = polib.pofile(opt['file'])
+        docObj = None
+        if Document.objects.filter(Name__exact=BaseName).count() > 0: # Check whether this doc has been inserted or not
+            docObj = Document.objects.get(Name__exact=BaseName)
+            if not opt['isUpdate']:
+                logging.warning("PO文件已存在，跳过添加")
                 sys.exit(0)
             
-        fileobj = file(open(opt['file'], 'r'), name=basename)
-        if docobj == none: # construct a new doc obj
-            docobj = document()
+        FileObj = File(open(opt['file'], 'r'), name=BaseName)
+        if docObj == None: # Construct a new doc obj
+            docObj = Document()
 
-        docobj.name = basename
-        docobj.pofile = fileobj
-        docobj.metaInfo = json.dumps(pofile.metadata)
-        docobj.save() # update or insert 
+        docObj.Name = BaseName
+        docObj.PoFile = FileObj
+        docObj.save() # Update or Insert 
 
-        if opt['isupdate']:
-            queryset = poentry.objects.filter(doc__name__exact=docobj.name)
+        if opt['isUpdate']:
+            queryset = POEntry.objects.filter(Doc__Name__exact=docObj.Name)
             logging.info("删除原有条目(total:%d)"%(len(queryset)))
             queryset.delete()
 
-        total = len(pofile)
+        total = len(PoFile)
         idx = 0
         with transaction.atomic():
-            for entry in pofile:
+            for entry in PoFile:
                 idx += 1
                 logging.debug("添加翻译条目(%d/%d)" % (idx, total))
-                poentry(raw=pickle.dumps(entry),
-                        doc=docobj,
-                        translated=entry.translated(),
-                        msgid=entry.msgid,
-                        msgstr=entry.msgstr
+                POEntry(Raw=pickle.dumps(entry),
+                        Doc=docObj,
+                        Translated=entry.translated(),
+                        Msgid=entry.msgid,
+                        Msgstr=entry.msgstr
                         ).save()
 
 
