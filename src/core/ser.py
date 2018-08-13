3d4843c21e8b66b5747bc713573d5127eebbf50d
LiuHancheng <2463765697@qq.com>
2018-08-13 23:40:02 +0800

    适配前端

--- src/core/ser.py
+++ src/core/ser.py
@@ -1,25 +1,31 @@
-from rest_framework import serializers 
-from core.models import POEntry,Document
+from rest_framework import serializers
+from core.models import POEntry, Document
+
 
 class POEntrySerializers(serializers.ModelSerializer):
     class Meta:
-        model =  POEntry
-        fields=('id','Translated','Msgid','Msgstr')
-    
+        model = POEntry
+        fields = ('id', 'Translated', 'Msgid', 'Msgstr')
+
+
 class DocMetaSerializers(serializers.Serializer):
-    TotalEntries= serializers.IntegerField()
+    TotalEntries = serializers.IntegerField()
     UntranslatedEntries = serializers.IntegerField()
 
+
 class DOcsSerializers(serializers.ModelSerializer):
+    """
+    TODO:新加入的extra field没有写测试
+    """
+    TotalEntries = serializers.SerializerMethodField()
+    UntranslatedEntries = serializers.SerializerMethodField()
+
     class Meta:
-        model=Document
-        fields =('id','Name')
-    
-    
-    
+        model = Document
+        fields = ('id', 'Name', 'TotalEntries', 'UntranslatedEntries')
 
-    
+    def get_TotalEntries(self, obj):
+        return obj.po_entries.all().count()
 
-    
-
-    
+    def get_UntranslatedEntries(self, obj):
+        return obj.po_entries.all().filter(Translated=False).count()
--- src/core/views.py
+++ src/core/views.py
@@ -99,7 +99,6 @@ class ListDocs(ListAPIView):
 
 
 
-
     
 
 
