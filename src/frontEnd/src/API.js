5ebff0835ef041c1e30ebab06d902a3ee0fa0a33
LiuHancheng <2463765697@qq.com>
2018-08-14 19:10:21 +0800

    Use auto-change API table

--- src/frontEnd/src/API.js
+++ src/frontEnd/src/API.js
@@ -1,10 +1,15 @@
-const BaseURL = 'http://localhost:8000/api'
+var API_URL = {
+    production: 'http://future.cnworkshop.xyz/api',
+    development: 'http://localhost:8000/api'
+}
+var environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const BaseURL = API_URL[environment]
 const APIList = {
     fetchUntranslated: BaseURL + "/listentry/cvpcb?untranslated=true",
     ListEntries: (docName, page, settings) => BaseURL + "/listentry/" + docName + "?page=" + page + '&' + Object.entries(settings).map(e => e.join('=')).join('&'),
     submitMsgstr: (docname, id) => BaseURL + "/entry/" + docname + "/" + id.toString(),
     getSingleEntry: (docname, id) => BaseURL + "/entry/" + docname + "/" + id.toString(),
     FetchdocMeta: (docname) => BaseURL + "/docMeta/" + docname,
-    getDocList : BaseURL + "/listdocs/",
+    getDocList: BaseURL + "/listdocs/",
 };
 export default APIList;
