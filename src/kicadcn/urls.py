1e9b6f5730bf0f6d4dfb742ff9997318674df09c
LiuHancheng <2463765697@qq.com>
2018-08-14 19:16:31 +0800

    Change project name

--- src/kicadcn/urls.py
+++ src/kicadcn/urls.py
@@ -1,4 +1,4 @@
-"""pogether URL Configuration
+"""kicadcn URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -19,6 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('core.urls')),
-
     path('user/',include('user.urls'))
 ]
